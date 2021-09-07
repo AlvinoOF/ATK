b92bb428bebd293dd5ea4e8d612d8fb90d1ba7dd
AlvinoOF <86650193+AlvinoOF@users.noreply.github.com>
2021-09-07 21:19:26 +0700

    Add files via upload

--- app/Controllers/PermintaanUMK.php
+++ app/Controllers/PermintaanUMK.php
@@ -28,56 +28,51 @@ class PermintaanUMK extends BaseController
         return view('permintaanumk/index', $data);
     }
 
-    public function create()
+    public function tambah_umk()
     {
         $data = [
             'title' => 'Form Tambah Permintaan UMK',
             'validation' => \Config\Services::validation()
         ];
 
-        return view('permintaanumk/create', $data);
+        return view('permintaanumk/tambah_umk', $data);
     }
 
-    public function sendEmail()
+    // public function sendEmail()
+    // {
+    //     $email = \Config\Services::email();
+    //     $id_user = $this->request->getVar('id_user');
+
+    //     $email->setTo('derago118@gmail.com');
+    //     $email->setFrom('johndoe@gmail.com');
+
+    //     $email->setSubject("Invoice Permintaan UMK");
+    //     $email->setMessage("id user" . $id_user);
+
+    //     if ($email->send() == TRUE) {
+    //         echo 'Email successfully sent';
+    //     } else {
+    //         echo "email gagal kirim";
+    //     }
+
+    //     $email->send();
+    // }
+
+    public function save_tambah_umk()
     {
-        $email = \Config\Services::email();
-        $id_user = $this->request->getVar('id_user');
-
-        $email->setTo('derago118@gmail.com');
-        $email->setFrom('johndoe@gmail.com');
-
-        $email->setSubject("Invoice Permintaan UMK");
-        $email->setMessage("id user" . $id_user);
-
-        if ($email->send() == TRUE) {
-            echo 'Email successfully sent';
-        } else {
-            echo "email gagal kirim";
-        }
-
-        $email->send();
-    }
-
-    public function save()
-    {
-        $this->sendEmail();
-        $id_user = $this->request->getVar('id_user');
-        $no_umk = $this->request->getVar('no_umk');
+        $id = $this->request->getVar('id');
+        $no_erp = $this->request->getVar('no_erp');
         $tgl_umk = $this->request->getVar('tgl_umk');
-        $batas_pumk = $this->request->getVar('batas_pumk');
         $user = $this->request->getVar('user');
-        $jumlah = $this->request->getVar('jumlah');
-        $sisa = $this->request->getVar('sisa');
+        $jumlah_umk = $this->request->getVar('jumlah_umk');
         $status = $this->request->getVar('status');
 
         $this->PermintaanUMKModel->save([
-            'id_user'    => $id_user,
-            'no_umk'     => $no_umk,
+            'id'    => $id,
+            'no_erp'     => $no_erp,
             'tgl_umk'    => $tgl_umk,
-            'batas_pumk' => $batas_pumk,
             'user'       => $user,
-            'jumlah'     => $jumlah,
-            'sisa'       => $sisa,
+            'jumlah_umk' => $jumlah_umk,
             'status'     => $status
         ]);
 
@@ -86,52 +81,52 @@ class PermintaanUMK extends BaseController
         return redirect()->to('/permintaanumk');
     }
 
-    public function edit($id)
+    public function terima_umk($id)
     {
         $data = [
             'title' => 'Form Edit Permintaan UMK',
             'validation' => \Config\Services::validation(),
-            'tbl_umk' => $this->PermintaanUMKModel->getDetailPermintaan($id)
+            'tbl_umk' => $this->PermintaanUMKModel->getTerimaUMK($id)
         ];
 
         $db      = \Config\Database::connect();
         $builder = $db->table('tbl_umk');
-        $query = $builder->select('id', 'no_erp', 'tgl_umk', 'batas_pumk', 'user', 'jumlah', 'sisa', 'status')
+        $query = $builder->select('id', 'no_erp', 'tgl_umk', 'batas_pumk', 'user', 'jumlah_umk', 'sisa', 'status')
             ->where('id', $id)->get();
 
         $data['permintaanumk'] = $query->getRow();
 
-        return view('permintaanumk/edit', $data);
+        return view('permintaanumk/terima_umk', $data);
     }
 
-    public function update($id)
-    {
-        $db      = \Config\Database::connect();
-        $builder = $db->table('tbl_umk');
+    // public function update($id)
+    // {
+    //     $db      = \Config\Database::connect();
+    //     $builder = $db->table('tbl_umk');
 
-        $data = [
-            'id'         => $id,
-            'no_erp'     => $this->request->getVar('no_erp'),
-            'tgl_umk'    => $this->request->getVar('tgl_umk'),
-            'batas_pumk' => $this->request->getVar('batas_pumk'),
-            'user'       => $this->request->getVar('user'),
-            'jumlah'     => $this->request->getVar('jumlah'),
-            'sisa'       => $this->request->getVar('sisa'),
-            'status'     => $this->request->getVar('status')
-        ];
+    //     $data = [
+    //         'id'         => $id,
+    //         'no_erp'     => $this->request->getVar('no_erp'),
+    //         'tgl_umk'    => $this->request->getVar('tgl_umk'),
+    //         'batas_pumk' => $this->request->getVar('batas_pumk'),
+    //         'user'       => $this->request->getVar('user'),
+    //         'jumlah_umk' => $this->request->getVar('jumlah_umk'),
+    //         'sisa'       => $this->request->getVar('sisa'),
+    //         'status'     => $this->request->getVar('status')
+    //     ];
 
-        $builder->replace($data);
+    //     $builder->replace($data);
 
-        return redirect()->to('/permintaanumk');
-    }
+    //     return redirect()->to('/permintaanumk');
+    // }
 
-    public function delete($id)
-    {
-        $db      = \Config\Database::connect();
-        $builder = $db->table('tbl_umk');
+    // public function delete($id)
+    // {
+    //     $db      = \Config\Database::connect();
+    //     $builder = $db->table('tbl_umk');
 
-        $builder->delete(['id' => $id]);
+    //     $builder->delete(['id' => $id]);
 
-        return redirect()->to('/permintaanumk');
-    }
+    //     return redirect()->to('/permintaanumk');
+    // }
 }
